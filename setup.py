a6fc07cfc66b43bf7a055163742f68c4de472c2a
philipsgn <tanphat260705@gmail.com>
2025-12-16 15:13:40 +0700

    logging and exception

--- setup.py
+++ setup.py
@@ -14,11 +14,12 @@ def get_requirements(file_path:str)->List[str]:
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
     return requirements
+# Dang ky project
 setup(
     name ='ML-project',
     version ='0.0.1',
     author='Philip',
     author_email='tanphat260705@gmail.com',
-    packages= find_packages(),
+    packages= find_packages(),      # Tim thu muc co file __init__.py --> Cac file nam cung co the import lan nhau
     install_requires= get_requirements('requirements.txt')
 )
