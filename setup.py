3db7ccc6f3f1dcf3c2ef550d163b82ea0fc4f141
philipsgn <tanphat260705@gmail.com>
2025-12-18 16:12:51 +0700

    EDA and Problems Statement

--- setup.py
+++ setup.py
@@ -1,25 +1,31 @@
-from setuptools import find_packages,setup
+from setuptools import find_packages, setup
 from typing import List
 
 HYPEN_E_DOT = "-e ."
-def get_requirements(file_path:str)->List[str]:
-    '''
-    this function return the list of requirements
-    '''
-    requirements=[]
-    with open('requirements.txt') as file_obj:
-        requirements= file_obj.readlines()
-        requirements= [req.replace("\n","") for req in requirements]
 
+def get_requirements(file_path: str) -> List[str]:
+    '''
+    This function returns the list of requirements
+    '''
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        # Strip whitespace và loại bỏ dòng trống
+        requirements = [req.strip() for req in requirements]
+        requirements = [req for req in requirements if req]  # Loại bỏ dòng trống
+        
+        # Loại bỏ '-e .'
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
+    
     return requirements
-# Dang ky project
+
+# Đăng ký project
 setup(
-    name ='ML-project',
-    version ='0.0.1',
+    name='ML-project',
+    version='0.0.1',
     author='Philip',
     author_email='tanphat260705@gmail.com',
-    packages= find_packages(),      # Tim thu muc co file __init__.py --> Cac file nam cung co the import lan nhau
-    install_requires= get_requirements('requirements.txt')
+    packages=find_packages(),  # Tìm thư mục có file __init__.py
+    install_requires=get_requirements('requirements.txt')
 )
